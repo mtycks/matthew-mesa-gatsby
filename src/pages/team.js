114f4535563b0694dc994ec1c5c4c311dbd1e839
Matthew-Mesa <matthewmesa@gmail.com>
2019-10-06 14:40:55 -0700

    added bootstrap and reactstrap blugins and created a navbar

---
+++ src/pages/team.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Our Team" />
+    <h1>Our Team</h1>
+  </Layout>
+)
+
+export default TeamPage
