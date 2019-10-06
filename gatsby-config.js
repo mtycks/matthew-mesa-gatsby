be11f3079bc8e88d85e046b2b359299448820acd
Matthew-Mesa <matthewmesa@gmail.com>
2019-10-06 14:00:17 -0700

    deleted boilerplate files, changed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Matt's first Gatsby.js Site`,
+    description: `Learning how to build a site using Gatsby.js!`,
     author: `@gatsbyjs`,
   },
   plugins: [
