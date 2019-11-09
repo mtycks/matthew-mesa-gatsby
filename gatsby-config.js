9a57f1611dbc01270a99a1c72e6dc14044fcfb4d
Matthew-Mesa <matthewmesa@gmail.com>
2019-11-08 20:33:30 -0800

    Updated plugin-manifest short_name and bg colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,10 +35,10 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `matthewmesa.com`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#252734`,
+        theme_color: `#252734`,
         display: `minimal-ui`,
         icon: `src/images/mm_icon.png`, // This path is relative to the root of the site.
       },
