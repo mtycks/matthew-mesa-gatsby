56e38127fce493f5ba1cdb65b34538a0c6efa5e8
Matthew-Mesa <matthewmesa@gmail.com>
2019-11-08 20:12:25 -0800

    Enabled plugin-offline for PWA'

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,6 @@ module.exports = {
     `gatsby-plugin-twitter`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
