8fb11b45507f89d090f4b54554fb4a9232bbc7a6
Matthew-Mesa <matthewmesa@gmail.com>
2020-05-12 09:06:10 -0700

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,23 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {Container, Row, Col} from 'reactstrap'
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout secondary="true">
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <section id="top" className="portfolio-item" style={{padding:"1.5rem"}}>&nbsp;</section>
+
+    <Container>
+      <Row>
+        <Col className="text-center pb-5 pt-5">
+          <h1 className="museo" style={{color:"#353535", fontSize:"20rem"}}>404</h1>
+          <p className="lead">Hmm, looks like this page doesn't exist.</p>
+        </Col>
+      </Row>
+    </Container>
+    
   </Layout>
 )
 
