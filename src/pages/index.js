0bce2f78a0bb14b4830cfc11d9db0cd02daddabe
Matthew-Mesa <matthewmesa@gmail.com>
2020-05-22 09:31:50 -0700

    Added profile image to homepage; added border-radius to a bunch of elements for consistency across the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,11 @@ import Layout from "../components/layout"
 import MMSEO from "../components/seo_mm"
 import {graphql, StaticQuery, Link} from 'gatsby'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Row, Col, Button} from 'reactstrap'
+import { Row, Col, Container} from 'reactstrap'
 import Img from 'gatsby-image'
 import { slugify } from '../../util/utilityFunctions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faInstagram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const IndexPage = ({ data }) => (
 
@@ -44,7 +46,7 @@ const IndexPage = ({ data }) => (
 	</section>
 
 
-  <div className="container pt-3 pb-3">
+  <Container id="hp_main_content" className="py-5">
 
     <MMSEO 
       title="Home"
@@ -54,62 +56,67 @@ const IndexPage = ({ data }) => (
 
     <Row>
 
-    <Col>
+      <Col>
 
-      <Row>
-        <Col md={{size:8, offset:2}}>
+        <div id="hp_intro__wrapper" className="mb-4">
+          <Row id="hp_intro">
+            <Col lg={{size:"3", offset:"0"}} md={{size:"4", offset:"4"}} xs={{size:"6", offset:"3"}} className="hp_intro__image">
+              <Img alt="Matthew Mesa smiling" fluid={data.mm_profile.childImageSharp.fluid} className="rounded-circle" />
+            </Col>
+            <Col lg={{size:"9"}} md={{size:"12"}} className="hp_intro__text">
 
-          <div className="text-center pt-2">
+              <h3 className="museo-700">Hi, I'm Matthew Mesa and I'm a free agent.</h3>
+              <p>Professionally, I’ve been a web developer, social media manager, graphic designer, photographer, video editor and writer.</p>
+              <p>Personally, I'm a husband and father of two who just really loves to create cool stuff—hopefully my wife and kids never get tired of me pointing a camera in their faces.</p>
+              <p><Link to="/about" className="btn btn-primary">About Me</Link> <Link to="/resume" className="btn btn-primary ml-1 mx-2">My Resume</Link> <br className="d-block d-lg-none" /> <a href="https://github.com/mtycks" target="_blank" rel="noreferrer" className="icon-link"><FontAwesomeIcon icon={faGithub} size="lg" /></a> <a href="https://twitter.com/mtycks" target="_blank" rel="noreferrer" className="icon-link"><FontAwesomeIcon icon={faTwitter} size="lg" /></a> <a href="https://instagram.com/mtycks" target="_blank" rel="noreferrer" className="icon-link"><FontAwesomeIcon icon={faInstagram} size="lg" /></a> <a href="https://www.linkedin.com/in/matthew-mesa-0bb8b626/" target="_blank" rel="noreferrer" className="icon-link"><FontAwesomeIcon icon={faLinkedin} size="lg" /></a> </p>
 
-            <p className="lead">Hi, I'm Matthew Mesa and <br className="d-block d-sm-none" />I'm a free agent.</p>
-            <p>Professionally, I’ve been a web developer, social media manager, graphic designer, photographer, video editor and writer. I used to work for the Los Angeles Dodgers and Zuno Studios, a small web design studio based in Signal Hill, CA.</p>
-            <p>Personally, I'm a husband and father who just really loves to create cool stuff—hopefully my wife and kids never get tired of me pointing a camera in their faces.</p>
+            </Col>
+          </Row>
+        </div>
 
-          </div>
+        <section id="hp_portfolio">
+          <StaticQuery query={indexQuery} render={data => {
 
+            return (
+              <Row>
 
-        </Col>
-      </Row>
-
-      <section id="hp_portfolio">
-        <StaticQuery query={indexQuery} render={data => {
-
-          return (
-            <Row>
-
-              {data.allMarkdownRemark.edges.map(({ node }, index) =>(
-
-                <Col key={index} xs={{ size: 6, offset: 0 }} md={{ size: 3, offset: 0 }}>
-                  <Link to={`/portfolio/${slugify(node.fields.slug)}`} className="portfolio-item">			    
-                    <Img alt={node.frontmatter.title} fluid={node.frontmatter.thumbnail.childImageSharp.fluid} />
-                  </Link>
+                <Col xs="12">
+                  <h2 className="text-center museo-700 mb-5">Portfolio</h2>
                 </Col>
 
+                {data.allMarkdownRemark.edges.map(({ node }, index) =>(
 
-                // <Post title={node.frontmatter.title}
-                //   author={node.frontmatter.author}
-                //   slug={node.fields.slug}
-                //   date={node.frontmatter.date}
-                //   body={node.excerpt} 
-                //   fluid={node.frontmatter.image.childImageSharp.fluid}
-                //   tags={node.frontmatter.tags}
-                // />
-
-              ))}
-            
-            </Row>
-          )
-
-          }}>
+                  <Col key={index} xs={{ size: 6, offset: 0 }} md={{ size: 3, offset: 0 }}>
+                    <Link to={`/portfolio/${slugify(node.fields.slug)}`} className="portfolio-item">			    
+                      <Img alt={node.frontmatter.title} fluid={node.frontmatter.thumbnail.childImageSharp.fluid} />
+                    </Link>
+                  </Col>
 
 
-        </StaticQuery>
-      </section>
+                  // <Post title={node.frontmatter.title}
+                  //   author={node.frontmatter.author}
+                  //   slug={node.fields.slug}
+                  //   date={node.frontmatter.date}
+                  //   body={node.excerpt} 
+                  //   fluid={node.frontmatter.image.childImageSharp.fluid}
+                  //   tags={node.frontmatter.tags}
+                  // />
+
+                ))}
+              
+              </Row>
+            )
+
+            }}>
+
+
+          </StaticQuery>
+        </section>
 
       </Col>
 
     </Row>
-  </div>
+  </Container>
 
 
   </Layout>
@@ -184,6 +191,14 @@ query{
   wide_cutout: file(relativePath: { eq: "header_wide_bg_cutout.png" }) {
     childImageSharp {
       fluid(maxWidth: 1200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+
+  mm_profile: file(relativePath: { eq: "mm_profile.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 500) {
         ...GatsbyImageSharpFluid
       }
     }
